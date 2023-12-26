ea43d0e45cd7ab3a5551c89d2bcb334626831918
Wdy234256 <wangzian@sheridancollege.ca>
2023-12-26 16:36:43 -0500

    reactdemo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+import logo from "../src/image/logo512.png"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <>
+    <nav className="header">
+        <img src={logo}></img>
+       <div> 
+        <a>First</a>
+        <a>Second</a>
+        </div>
+    </nav>
+    <h3>Having fun</h3>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+    </ul>
+    </>
+    ,
+    document.getElementById("root")
+)
